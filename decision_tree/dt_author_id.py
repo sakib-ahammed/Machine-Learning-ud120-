8bb00e83b1504ad357764a167927034415979ff6
sakib-ahammed <sakib.kuet10@gmail.com>
2017-02-05 11:54:02 +0600

    DsT quiz question answer

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,11 +20,18 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+clf=tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred=clf.predict(features_test)
 
+print "Start...."
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print acc
+print "Completed.."
 
 #########################################################
 
